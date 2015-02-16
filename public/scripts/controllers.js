c9aae79b82c18c431c8b14bd81e209df79fb0a75
Greg <ggundersen@gmail.com>
2015-02-16 18:08:19 -0500

    Cleanup and style

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -5,8 +5,33 @@ indexControllers.controller('tableCtrl', ['$scope', '$modal', 'centerUrls', 'get
 
 		getSource.then(function(source){
 			$scope.docs = source.transformed;
-			$scope.summary = source.summary;
+			$scope.summary = {
+			    center: 0,
+		        assays: 0,
+		        cellLines: 0,
+		        perturbagens: 0
+			};
 			console.log($scope.docs);
+
+            function countUpTo(field, count, max, step, time) {
+                setTimeout(function() {
+                    if (count === max) {
+                        return;
+                    } else if (count + step > max) {
+                        countUpTo(field, count, max, 1, 0);
+                    } else {
+                        count = count + step;
+                        $scope.summary[field] = count;
+                        $scope.$apply();
+                        countUpTo(field, count, max, step, time);
+                    }
+                }, time);
+            }
+
+            countUpTo('center', 0, source.summary.center, 1, 50);
+            countUpTo('assays', 0, source.summary.assays, 1, 10);
+            countUpTo('perturbagens', 0, source.summary.perturbagens, 50, 10);
+            countUpTo('cellLines', 0, source.summary.cellLines, 5, 10);
 		});
 
 		$scope.centerUrls = centerUrls;
