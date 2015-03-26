8d4bfcb83ec8211b7dcafa16a395e8f658cab790
Michael McDermott <michael.mcdermott@mssm.edu>
2015-03-26 11:23:29 -0400

    Updated product.html to conform to Wordpress

--- query-mongo.js
+++ query-mongo.js
@@ -1,25 +1,31 @@
- var MongoClient = require('mongodb').MongoClient
- var q = require('q')
+ var MongoClient = require('mongodb').MongoClient;
+ var q = require('q');
 
 var coll;
 var connectDeferred = q.defer();
 // point to falafel server
 MongoClient.connect('mongodb://readWriteUser:askQiaonan@10.91.53.62/LINCS',
 	function(err,db){
-		if(err) connectDeferred.reject(err);
-		else connectDeferred.resolve(db.collection("milestones"));
-		coll = db.collection("milestones")
+		if(err) {
+            connectDeferred.reject(err);
+        } else {
+            connectDeferred.resolve(db.collection("milestones"));
+        }
+		coll = db.collection("milestones");
 		
 		
 		exports.getAll=function(){
 			var deferred = q.defer();
 			coll.find().sort({center:1,"assay_info":1})
 				.toArray(function(err,results){
-					if(err) deferred.reject(err);
-					else deferred.resolve(results);
+					if(err) {
+                        deferred.reject(err);
+                    } else {
+                        deferred.resolve(results);
+                    }
 			});
 			return deferred.promise;
-		}
+		};
 		
 });
 
