190e21eae41b113f057b1426ef984b90272ebf77
skoplev <skoplev@gmail.com>
2016-03-24 19:16:56 -0400

    Infered grid.css file from minimized file.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,7 @@
 var Milestones = angular.module('Milestones', [
   'indexControllers', 'smart-table', 'ui.bootstrap']);
 
-// Milestones.config(["$controllerProvider", function($controllerProvider) {
-//   $controllerProvider.allowGlobals();
-// }]);
+// var Milestones = angular.module('Milestones', [
+//   'indexControllers', 'smart-table']);
+
+
